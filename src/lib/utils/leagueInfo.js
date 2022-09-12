3d398e479141457c7180369cb9b3d1c193de65ef
coop92 <ccooper@sonicu.com>
2022-09-12 16:42:39 -0400

    updated dues

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "783723162142658560"; // your league ID
 export const leagueName = "All Gold Everything"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 70; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
