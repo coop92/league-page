832cdd48599b7efe55124256cb61ae06d0fc8ea7
coop92 <ccooper@sonicu.com>
2022-09-13 08:18:18 -0400

    blog support added

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,8 +3,9 @@ export const leagueID = "783723162142658560"; // your league ID
 export const leagueName = "All Gold Everything"; // your league name
 export const dues = 70; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
-
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const VITE_CONTENTFUL_ACCESS_TOKEN = YAbLczuSov_ax5ibNeqjAzXPrGMsbkiF7zQoJrMStkE;
+export const VITE_CONTENTFUL_SPACE = oxsjhrkmtyap;
 /*   STEP 2   */
 export const homepageText = `
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
